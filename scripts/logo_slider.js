0f4df45db6d962dd37c4bcc8862b359052375b47
Sadia Afridi <sadiaafridi663@gmail.com>
2025-08-13 21:11:10 +0500

    Update Swiper JS

--- scripts/logo_slider.js
+++ scripts/logo_slider.js
@@ -1,22 +1,32 @@
- 
-            var swiper = new Swiper(".mySwiper", {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,  // Default for smallest screens
+        spaceBetween: 16,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        grabCursor: true,
+        breakpoints: {
+            // When window width is >= 480px
+            480: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            // When window width is >= 640px
+            640: {
                 slidesPerView: 4,
-                spaceBetween: 0, // consistent gap
-                loop: true,
-                autoplay: {
-                    delay: 2000,
-                    disableOnInteraction: false,
-                },
-                grabCursor: true,
-                breakpoints: {
-                    320: {
-                        slidesPerView: 2
-                    },
-                    640: {
-                        slidesPerView: 3
-                    },
-                    1024: {
-                        slidesPerView: 5
-                    }
-                }
-            });
+                spaceBetween: 24
+            },
+            // When window width is >= 1024px
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 32
+            },
+            // When window width is >= 1280px
+            1280: {
+                slidesPerView: 6,
+                spaceBetween: 32
+            }
+        }
+    });
